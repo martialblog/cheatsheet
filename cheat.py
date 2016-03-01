5d22eb89e14fdda1ea2821e5d4ef0e4e6202d20c
Markus <markus@martialblog.de>
2016-03-01 17:37:18 +0100

    Added command line arguments to format output

--- cheat.py
+++ cheat.py
@@ -1,55 +1,59 @@
-#/bin/python3
+#!/usr/bin/python3
 import sys
-import getopt
+import argparse
 import os
 import io
 import configparser
 
 # TODO
-# Parse command line options and shit
- # -l lines
- # Add file to staging ares: git add filename
- # -b break
- # Add file to stating area:
- # git add filename
+# Check if INI syntax is OK
+# Colored or formated output... better readable anyways
 
+desc = "Cool Command-Line Cheatsheets"
 extension = ".ini"
 cheatsheets = []
 configDir = "."
-cfg = configparser.ConfigParser()
+cfgParser = configparser.ConfigParser()
+argParser = argparse.ArgumentParser(description=desc)
+
+#Command Line Arguments
+group = argParser.add_mutually_exclusive_group()
+group.add_argument("-l", "--inline", action="store_true", help="Output each line, this is default")
+group.add_argument("-b", "--breakline", action="store_true", help="Output break line")
+argParser.add_argument("cheatsheet", help="The cheatsheet you want to see")
 
 def printInline():
     #print inline
-    for key in cfg['cheats']:
-        print(key +": "+ cfg['cheats'][key])
+    for key in cfgParser['cheats']:
+        print(key +": "+ cfgParser['cheats'][key])
 
 def printBreakLine():
     #print breakline
-    for key in cfg['cheats']:
-        print("\n"+ key +":\n"+ cfg['cheats'][key])
+    for key in cfgParser['cheats']:
+        print("\n"+ key +":\n"+ cfgParser['cheats'][key])
 
 def createFileList(extension):
     for file in os.listdir(configDir):
         if file.endswith(extension):
             cheatsheets.append(file)
 
-def checkAvailableCheatSheets(name):
-    isAvailable = False
-    #make list of available .cs
-    #check if name exists
-    return isAvailable
-
 def main():
-    #ParseCommandLineParameter
-    #MakeFileList
-    #Parse shit
-    #Is argv1 available
-    #print shit
+    args = argParser.parse_args()
+    filename = args.cheatsheet + extension
     createFileList(extension)
 
-    cfg.read('git.ini')
-    printInline()
-    printBreakLine()
+    if not os.path.exists(filename):
+        print("No such file:", filename)
+        exit(1)
+    else:
+        cfgParser.read(filename)
+
+    if args.inline:
+        printInline()
+    elif args.breakline:
+        printBreakLine()
+    else:
+        printInline()
 
 if __name__ == "__main__":
     main()
