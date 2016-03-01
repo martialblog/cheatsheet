857f76f09cd88b45828831d1a9c807a1bc0b6bd2
Markus <markus@martialblog.de>
2016-03-01 11:12:25 +0100

    Initial commit

---
+++ cheat.py
@@ -0,0 +1,13 @@
+#/bin/python3
+
+#TODO
+#check directory for .cs files
+#check format of .cs files
+#Console parameter = filename oder name=cheatsheet name
+
+# OUTPUT
+# user@terminal$ cheat.py git
+# Add file to staging ares: git add filename
+# ODER?
+# Add file to stating area:
+# git add filename
---
+++ example.cs
@@ -0,0 +1,5 @@
+#Cheatsheet example for git
+
+"name": "git"
+"desc": "My cool git cheatsheet"
+"Add file to staging area": "git add filename"
