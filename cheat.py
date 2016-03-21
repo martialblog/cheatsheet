21ef2cc736446c42e22ca2acfb97870460ff116b
Markus <markus@martialblog.de>
2016-03-21 16:45:00 +0100

    Printer Abstraction added.
    Now a now style can just be added by adding a new Printer class and commandline argument. Without touching anything else.

--- cheat.py
+++ cheat.py
@@ -5,45 +5,48 @@ from sys import path
 from sys import exit
 from os import listdir
 
-class Printer():
+class Printer:
 
     def __init__(self, configparser):
         self._configparser = configparser
-        self._width = 10
-        self.calculateDescriptionWidth()
-
-    def getWidth():
-        return self._width
-
-    def calculateDescriptionWidth(self):
-        """
-        Returns the width of the longest description in the cheatsheet, so that the output can be dynamic.
-        """
-
-        for description in self._configparser['cheats']:
-            if len(description) > self._width:
-                self._width = len(description)
-
-class InlinePrinter(Printer):
 
     def printCheatSheet(self):
-        """
-        Prints the cheatssheet inline, so that it's grep-able.
-        """
+        raise NotImplementedError
+
+class InlinePrinter(Printer):
+    """
+    Prints the cheatssheet inline, so that it's grep-able.
+    """
+
+    def __init__(self, configparser):
+        super().__init__(configparser)
+        self._width = self.calculateDescriptionWidth()
+
+    def calculateDescriptionWidth(self):
+        width = 10
 
         for description in self._configparser['cheats']:
+            if len(description) > width:
+                width = len(description)
+
+        return width
+
+    def printCheatSheet(self):
+        for description in self._configparser['cheats']:
             value = self._configparser['cheats'][description]
-            output = "{0:<{1}} {2}".format(description, self.getWidth(), value)
+            output = "{0:<{1}} {2}".format(description, self._width, value)
 
             print(output)
 
-class BreaklinePrinter(Printer)
+class BreaklinePrinter(Printer):
+    """
+    Prints the cheatsheet with newlines
+    """
+
+    def __init__(self, configparser):
+        super().__init__(configparser)
 
     def printCheatSheet(self):
-        """
-        Prints the cheatsheet with newlines
-        """
-
         for description in self._configparser['cheats']:
             value = self._configparser['cheats'][description]
             output = "{0} \n {1}".format(description, value)
@@ -51,27 +54,23 @@ class BreaklinePrinter(Printer)
             print(output)
 
 class CheatHandler:
+    """
+    Indexes the available INI files within the config folder and parses the requested file
+    """
 
     def __init__(self, extention, directory):
-
         self.__configParser = configparser.ConfigParser()
         self.__available_cheatsheets = {}
         self.__config_directory = directory
         self.__file_extension = extention
 
     def getConfigParser(self):
-
         return self.__configParser
 
     def printCheatsheetNotAvailable(self, cheatsheet):
-
         print(cheatsheet +' - Cheatsheet not available')
 
     def indexCheatsheets(self):
-        """
-        Indexes the available INI files within the config folder
-        """
-
         tempParser = configparser.ConfigParser()
 
         for filename in listdir(self.__config_directory):
@@ -86,10 +85,6 @@ class CheatHandler:
                 print(exception)
 
     def parseRequestedCheatSheet(self, requested_cheatsheet):
-        """
-        Parses the requested cheatsheet. If the cheatsheet isn't available the program exits with status 1
-        """
-
         if requested_cheatsheet in self.__available_cheatsheets.keys():
             path_to_file = self.__config_directory + self.__available_cheatsheets[requested_cheatsheet]
             self.__configParser.read(path_to_file)
@@ -110,17 +105,20 @@ def main():
     argumentParser = argparse.ArgumentParser(description=description)
     argumentParser.add_argument("cheatsheet", help=help_general)
     group = argumentParser.add_mutually_exclusive_group()
-    group.add_argument("-l", "--inline", action="store_const", help=help_inline)
-    group.add_argument("-b", "--breakline", action="store_const", help=help_breakline)
+    group.add_argument('-l', help=help_inline, action='store_const', dest='printer', const='InlinePrinter')
+    group.add_argument('-b', help=help_breakline, action='store_const', dest='printer', const='BreaklinePrinter')
     cmdArguments = argumentParser.parse_args()
 
-    #Initialize CheatHanlder
+    #instantiate CheatHandler
     chandler = CheatHandler(extention, directory)
     chandler.indexCheatsheets()
     chandler.parseRequestedCheatSheet(cmdArguments.cheatsheet)
 
-    #printer = either Break or Inline printer
-    #printer.printCheatSheet()
+    #instantiate CheatPrinter based on command-line argument.
+    printer_type = cmdArguments.printer
+    printer_constructor = globals()[printer_type]
+    cprinter = printer_constructor(chandler.getConfigParser())
+    cprinter.printCheatSheet()
 
     #Everything is fine and we can exit with 0
     exit(0)
