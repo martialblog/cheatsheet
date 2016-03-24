a2a9a56695377604e82ab56aea5347c398fd10a1
Markus <markus@martialblog.de>
2016-03-24 19:52:56 +0100

    Cleaned up printer class. Code looks way better... Maybe there's a way to get rid of the reduntant for loops?

--- cheat.py
+++ cheat.py
@@ -5,12 +5,10 @@ from sys import path
 from sys import exit
 from os import listdir
 
-#Lot or reduntant code here... gotta clean that up
-#Make more stable when file
 class Printer:
 
     def __init__(self, configparser):
-        self._configparser = configparser
+        self.configparser = configparser
 
     def printCheatSheet(self):
         raise NotImplementedError
@@ -20,23 +18,20 @@ class InlinePrinter(Printer):
     Prints the cheatssheet inline, so that it's grep-able.
     """
 
-    def __init__(self, configparser):
-        super().__init__(configparser)
-        self._width = self.calculateDescriptionWidth()
-
-    def calculateDescriptionWidth(self):
+    @property
+    def width(self):
         width = 10
-
-        for description in self._configparser['cheats']:
+        
+        for description in self.configparser['cheats']:
             if len(description) > width:
                 width = len(description)
 
         return width
 
     def printCheatSheet(self):
-        for description in self._configparser['cheats']:
-            value = self._configparser['cheats'][description]
-            output = "{0:<{1}} {2}".format(description, self._width, value)
+        for description in self.configparser['cheats']:
+            value = self.configparser['cheats'][description]
+            output = "{0:<{1}} {2}".format(description, self.width, value)
 
             print(output)
 
@@ -45,12 +40,9 @@ class BreaklinePrinter(Printer):
     Prints the cheatsheet with newlines
     """
 
-    def __init__(self, configparser):
-        super().__init__(configparser)
-
     def printCheatSheet(self):
-        for description in self._configparser['cheats']:
-            value = self._configparser['cheats'][description]
+        for description in self.configparser['cheats']:
+            value = self.configparser['cheats'][description]
             output = "{0} \n {1}".format(description, value)
 
             print(output)
@@ -66,7 +58,7 @@ def main():
 
     #COMMAND-LINE ARGUMENTS!
     argumentParser = argparse.ArgumentParser(description=description)
-    argumentParser.add_argument("cheatsheet", help=help_general)
+    argumentParser.add_argument('cheatsheet', help=help_general)
     group = argumentParser.add_mutually_exclusive_group()
     group.add_argument('-l', help=help_inline, action='store_const', dest='printer', const='InlinePrinter')
     group.add_argument('-b', help=help_breakline, action='store_const', dest='printer', const='BreaklinePrinter')
@@ -85,6 +77,8 @@ def main():
         cheatPrinter.printCheatSheet()
     except configparser.Error as exception:
         print(exception)
+    except:
+        print(filename + " not available or contains errors")
 
     exit(0)
 
