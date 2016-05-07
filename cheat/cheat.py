7a199930b09b6736d5b2266b40e932d7d84dab98
Markus Opolka <markus@martialblog.de>
2016-05-07 18:26:38 +0200

    Major Refactor - Moved Printer Classes to seperate file and cleaned up main

--- cheat/cheat.py
+++ cheat/cheat.py
@@ -13,79 +13,24 @@ Add the new command-line option to the printer group, like so:
 
 Add the new Printer type to the PrinterFactory.
 
-Then create a new Printer Subclass based on the name you just added. It should the implement the printCheatSheet method:
-    class MyNewPrinter(Printer):
+Then create a new Printer Subclass based on the name you just added.
+It should the implement the printCheatSheet method:
+
+class MyNewPrinter(Printer):
         [...]
 """
 
+
 import os
-from configparser import ConfigParser
 from argparse import ArgumentParser
-from sys import path
+from configparser import ConfigParser
+from printer import PrinterFactory
 from sys import exit
-
-class Printer:
-    """
-    Base class for the cheatsheet printers. Takes care of the actuall printing.
-
-    Args:
-    Takes a configparser objects to print.
-    """
-
-    def __init__(self, configparser):
-        self.configparser = configparser
-
-    # TODO: Try-Except
-    def printsheet(self, template):
-        for description in self.configparser['cheats']:
-            value = self.configparser['cheats'][description]
-            output = template.format(description, value)
-            print(output)
-
-
-class InlinePrinter(Printer):
-    """
-    Prints the cheatssheet line-by-line, so that it's grep-able.
-    """
-
-    @property
-    def width(self):
-        width = len(max(self.configparser['cheats'], key=len))
-
-        return str(width)
-
-    def printsheet(self):
-        print_format = "{0:<" + self.width + "} {1}"
-        super().printsheet(print_format)
-
-
-class BreaklinePrinter(Printer):
-    """
-    Prints the cheatsheet and breaks the line after the description.
-    """
-
-    # TODO Maybe use ljust rjust
-    def printsheet(self):
-        print_format = "{0} \n {1}"
-        super().printsheet(print_format)
-
-
-def PrinterFactory(name):
-    """
-    Creates a Printer object from the name given.
-    """
-
-    printer_classes = {
-        "InlinePrinter": InlinePrinter,
-        "BreaklinePrinter": BreaklinePrinter
-    }
-
-    return printer_classes[name]
 
 
 def main():
     # GENERAL SETTINGS!
-    directory = os.path.join(os.getcwd(), "config/")
+    directory = os.path.join(os.getcwd(), "sheets/")
     extention = ".ini"
     description = "Cool Command-line Cheatsheets"
     help_general = "The cheatsheet you want to see"
@@ -104,7 +49,7 @@ def main():
     # WHERE THE RUBBER MEETS THE ROAD!
     cmd_arguments = argumentparser.parse_args()
     filename = directory + cmd_arguments.cheatsheet + extention
-    CheatPrinterConstructor = PrinterFactory(cmd_arguments.printer)
+    CheatPrinterConstructor = PrinterFactory.create_printer(cmd_arguments.printer)
     configparser = ConfigParser()
     cheatprinter = CheatPrinterConstructor(configparser)
 
@@ -113,8 +58,10 @@ def main():
         cheatprinter.printsheet()
         exitcode = 0
     except Exception as e:
-        # I know lazy handling, but it works perfect... Sorry.
+        # I know lazy handling... Sorry.
+        # TOOD better exception handling.
         print(filename + " not available or contains errors.")
+        print(e)
         exitcode = 1
     finally:
         exit(exitcode)
