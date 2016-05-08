ade2a6f13bfe11822c5b8743446a1eef2626a41d
Markus Opolka <markus@martialblog.de>
2016-05-08 11:04:54 +0200

    Added more docstrings

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 
 
-"""
-Idea: Have example cheatsheet and then check if the classes to what they should?
-"""
-
-
 import sys
 sys.path.append('../cheat/')
-
-import unittest
-from unittest.mock import patch
 
 from configparser import ConfigParser
 from io import StringIO
 
+import unittest
+from unittest.mock import patch
+
+# Printer Classes that will be tested
 from printer import *
 
 
 class PrinterTest(unittest.TestCase):
-    """Basic test cases."""
+    """Some basic tests to check the Printer classes"""
 
     def setUp(self):
+        """
+        Setting up the test by reading the test cheatsheet with expected content to test against.
+        """
+
         self.cp = ConfigParser()
         self.cp.read("test.ini")
 
+    #TODO Test if the width calculation is alright
     def test_InlinePrinter(self):
         """
-        Testing if the InlinePrinter does its job
+        Testing if the InlinePrinter does its job.
         """
 
         #TODO Make this more readable
@@ -37,7 +38,6 @@ class PrinterTest(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as fake_out:
             printer.printsheet()
             self.assertEqual(fake_out.getvalue(), expected_output)
-
 
     def test_BreaklinePrinter(self):
         """
