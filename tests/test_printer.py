f480d9ed1f0628450ad1d625bae8a84010b7b264
Markus Opolka <markus@martialblog.de>
2016-05-08 11:01:18 +0200

    Implementet tests for the Printer classes

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,18 +1,71 @@
 #!/usr/bin/env python3
 
+
+"""
+Idea: Have example cheatsheet and then check if the classes to what they should?
+"""
+
+
 import sys
 sys.path.append('../cheat/')
-import printer
-
 
 import unittest
+from unittest.mock import patch
+
+from configparser import ConfigParser
+from io import StringIO
+
+from printer import *
 
 
-class PrinterTestSuite(unittest.TestCase):
+class PrinterTest(unittest.TestCase):
     """Basic test cases."""
 
-    def test_Printer(self):
-        assert True
+    def setUp(self):
+        self.cp = ConfigParser()
+        self.cp.read("test.ini")
+
+    def test_InlinePrinter(self):
+        """
+        Testing if the InlinePrinter does its job
+        """
+
+        #TODO Make this more readable
+        expected_output = "test cheat a lorem\ntest cheat b ipsum\ntest cheat c dolor\n"
+        printer = InlinePrinter(self.cp)
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            printer.printsheet()
+            self.assertEqual(fake_out.getvalue(), expected_output)
+
+
+    def test_BreaklinePrinter(self):
+        """
+        Testing if the BreaklinePrinter does its job
+        """
+
+        #TODO Make this more readable
+        expected_output = "test cheat a \n lorem\ntest cheat b \n ipsum\ntest cheat c \n dolor\n"
+        printer = BreaklinePrinter(self.cp)
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            printer.printsheet()
+            self.assertEqual(fake_out.getvalue(), expected_output)
+
+
+    def test_Printer_printsheet(self):
+        """
+        Testing if the printsheet for loop does its job
+        """
+
+        #TODO Make this more readable
+        expected_output = "test cheat a\ntest cheat b\ntest cheat c\n"
+        printer = Printer(self.cp)
+        template = "{0}"
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            printer.printsheet(template)
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
 
 if __name__ == '__main__':
